f9eb074a9962deb38cecd09dab65b22486675672
Real-Power-Creative <105161398+Real-Power-Creative@users.noreply.github.com>
2022-05-11 08:10:21 +0530

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -5,7 +5,7 @@ ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 
 # -SET LOCAL SETTINGS & SYSTEM LANGUAGE-
-sed -i '169s/.//' /etc/locale.gen
+sed -i 's/#en_IN UTF-8/en_IN UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_IN.UTF-8" >> /etc/locale.conf
 
