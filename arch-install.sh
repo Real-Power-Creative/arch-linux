4ca447eefaa74c59388af4e249e3d167b0859b2f
Real-Power-Creative <105161398+Real-Power-Creative@users.noreply.github.com>
2022-05-11 09:16:34 +0530

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -16,6 +16,7 @@ echo ::1		localhost >> /etc/hosts
 echo 127.0.1.1	real-arch.localdomain	real-arch >> /etc/hosts
 pacman -S networkmanager
 systemctl enable NetworkManager
+sed -i 
 
 # -ADD NEW USER-
 useradd -m -G wheel -s /bin/bash real-power
@@ -29,3 +30,5 @@ mount /dev/nvme0n1p1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
+#-CONSOLEFONT-
+echo "FONT=ter-d28b.psf.gz" >> /etc/vconsole.conf
